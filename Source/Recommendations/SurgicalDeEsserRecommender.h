8d190a2e0c9abc843ec0612b4833544fb9658c90
sergueyqqrr-lang <sergueyqqrr@gmail.com>
2026-09-24 04:08:29 -0400

    Delete Source directory

--- Source/Recommendations/SurgicalDeEsserRecommender.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "PluginRecommender.h"
-
-class SurgicalDeEsserRecommender : public PluginRecommender
-{
-public:
-    juce::String getTargetPluginName() const override { return "Surgical De-Esser"; }
-    std::vector<Recommendation> generate (const SoundProfile& profile) const override;
-};
